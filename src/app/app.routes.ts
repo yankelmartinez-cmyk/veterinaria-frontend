f0438da8232acdd368558bfd59c834261dec5444
ronyvasqueze <ronidanielvasque@gmail.com>
2026-04-12 11:48:26 -0600

    Agrega pagina admin-tipos-mascota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AdminCitas } from './pages/admin-citas/admin-citas';
 import { AdminVeterinarios } from './pages/admin-veterinarios/admin-veterinarios';
+import { AdminTiposMascota } from './pages/admin-tipos-mascota/admin-tipos-mascota';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'citas', pathMatch: 'full' },
   { path: 'citas', component: AdminCitas },
-  { path: 'veterinarios', component: AdminVeterinarios }
+  { path: 'veterinarios', component: AdminVeterinarios },
+  { path: 'tipos-mascota', component: AdminTiposMascota }
 ];
---
+++ src/app/pages/admin-tipos-mascota/admin-tipos-mascota.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminTiposMascota } from './admin-tipos-mascota';
+
+describe('AdminTiposMascota', () => {
+  let component: AdminTiposMascota;
+  let fixture: ComponentFixture<AdminTiposMascota>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AdminTiposMascota],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AdminTiposMascota);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
